1b4e2d9e06393fd1d9343fc06c9723e7ea91f4d5
余沛麟 <yuchacha250@gmail.com>
2020-01-14 14:24:52 +0800

    fix

--- src/views/ElementConfigure.js
+++ src/views/ElementConfigure.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import Icon from 'antd/es/icon';
-import DndElement from '../components/DndElement';
+import DndComp from '../components/DndComp';
 
 const ElementConfigure = React.forwardRef((props, ref) => {
   const { columns = [], onOpen, onDrop } = props;
   return (
     <div className='atc-config-area' ref={ref} data-drag-type='column'>
-      <div className='atc-group-title'>列表配置</div>
-      <div className='atc-config-tip'>
-        <Icon type='info-circle' className='atc-config-tip-icon' />
-        你可以通过拖放元素库中的元素来自定义列表
-      </div>
-      <div className='atc-column-container'>
-        {
-          columns.map((data, index) => (
-            <div className='atc-column' key={data.dataIndex}>
-              <div className='atc-column-index'>
-                {index + 1}
-                <Icon type='edit' className='atc-column-edit' onClick={onOpen.bind(this, data)} />
-              </div>
-              <DndElement onDrop={onDrop} element={data} className='atc-column-title'>
-                {data.title}
-              </DndElement>
+      {
+        columns.map((data, index) => (
+          <div className='atc-column' key={data.dataIndex}>
+            <div className='atc-column-index'>
+              {index + 1}
+              <Icon type='edit' className='atc-column-edit' onClick={onOpen.bind(this, data)} />
             </div>
-          ))
-        }
-      </div>
+            <DndComp
+              type='column'
+              className='atc-column-title'
+              element={data}
+              onDrop={onDrop}
+            >
+              {data.title}
+            </DndComp>
+          </div>
+        ))
+      }
     </div>
   );
 });
--- src/views/index.js
+++ src/views/index.js
@@ -5,32 +5,9 @@ import Icon from 'antd/es/icon';
 import ElementConfigure from './ElementConfigure';
 import ColumnModal from './ColumnModal';
 import { TableColumn } from '../core';
-import DndElement from '../components/DndElement';
+import DndComp from '../components/DndComp';
 import './style';
 import './index.less';
-
-const data = [{
-  title: '姓名',
-  dataIndex: 'patientName',
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-}, {
-  title: '出生日期',
-  dataIndex: 'birthday',
-}, {
-  title: '采样日期',
-  dataIndex: 'deliverDay',
-}, {
-  title: '报告日期',
-  dataIndex: 'reportDay',
-}, {
-  title: '检测方案',
-  dataIndex: 'prodcde',
-}, {
-  title: '样本类型',
-  dataIndex: 'sampleType',
-}];
 
 const Columns = TableColumn.initialize();
 
@@ -39,7 +16,7 @@ function AntdTableConfig(props) {
   const configureRef = useRef();
   const elementGroupRef = useRef();
   const atcLayoutRef = useRef();
-  const [columns, setColumns] = useState([]);
+  const [columns, setColumns] = useState(Columns.data || []);
   const [modalVisible, setModalVisible] = useState(false);
   const [currentColumn, setCurrentColumn] = useState({});
   const columnMap = columns.reduce((a, b) => { a[b.dataIndex] = true; return a; }, {});
@@ -79,10 +56,10 @@ function AntdTableConfig(props) {
 
   function moveColumn(layout, element) {
     if (elementGroupRef.current) {
-      const { x, y } = layout;
+      const { clientX, clientY } = layout;
       const configAreaRect = elementGroupRef.current.getBoundingClientRect();
-      if (configAreaRect.left <= x && configAreaRect.top <= y
-        && configAreaRect.right >= x && configAreaRect.bottom >= y) {
+      if (configAreaRect.left <= clientX && configAreaRect.top <= clientY
+        && configAreaRect.right >= clientX && configAreaRect.bottom >= clientY) {
         Columns.delete(element);
         updateColumns();
       }
@@ -107,21 +84,30 @@ function AntdTableConfig(props) {
         <div className='atc-group-title'>元素库</div>
         <div className='atc-elements-container'>
           {
-            (data || []).map((element) => (
-              <DndElement
+            (dataSource || []).map((element) => (
+              <DndComp
                 key={element.dataIndex}
                 className='atc-element'
                 element={element}
                 disable={columnMap[element.dataIndex]}
                 onDrop={addColumn}
+                type='element'
               >
                 {element.title}
-              </DndElement>
+              </DndComp>
             ))
           }
         </div>
       </section>
       <section className='atc-layout-content'>
+        <div className='atc-config-header'>
+          <div className='atc-group-title'>列表配置</div>
+          <div className='atc-config-tip'>
+            <Icon type='info-circle' className='atc-config-tip-icon' />
+            你可以通过拖放元素库中的元素来自定义列表
+            <div style={{ paddingLeft: 20 }}>添加的行数多余一行时，按第一行添加</div>
+          </div>
+        </div>
         <ElementConfigure
           ref={configureRef}
           columns={columns}
